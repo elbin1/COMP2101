d86b0c5f2a654d807dd4f9b3bb1e808595851d57
elbin1 <200466128@student.georgianc.on.ca>
2023-02-13 23:31:03 -0500

    lab 3

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,10 +1,29 @@
-#!/bin/bash 
-echo "System Information" 
-echo "FODN:"
-hostname -f 
-echo "Host Information." 
-cat /etc/*-release 
-echo "IP Addresses:" 
-hostname -I
-echo "Root Filesystem Status:"
-df /
+#!/bin/bash
+
+# hostname and FQDN
+HOSTNAME=$(hostname)
+FQDN=$(hostname -f)
+
+# OS info
+OS=$(lsb_release -ds | cut -d " " -f 1-3)
+
+# IP address
+IP=$(ip route get 1 | awk '{print $7}')
+
+# free disk space
+SPACE=$(df -h / | awk 'NR==2{print $4}')
+
+#Output template
+TEMPLATE=$(cat << EOF
+Report for $HOSTNAME
+===============
+FQDN: $FQDN
+Operating System name and version: $OS
+IP Address: $IP 
+Root Filesystem Free Space: $SPACE
+===============
+EOF
+)
+
+# Output
+echo -e "\n$TEMPLATE"
